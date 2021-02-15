7601125a5445e819054d888fe919b64673d04959
Solaire003 <vadin-abramov-00@mail.ru>
2021-02-15 17:08:20 +0300

    fix: date of message

--- src/db/models/message.model.js
+++ src/db/models/message.model.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const messageSchema = new Schema({
     content: { type: String, require: true, trim: true},
-    date: { type: Date, default: new Date()},
+    date: { type: Date, default: Date.now},
     author: { type: Schema.ObjectId, ref: "User"},
     read: {type: Boolean, default: false},
     video: {type: String, trim: true}
